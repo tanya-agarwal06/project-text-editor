4898a99db9df49f5f1b2ba9f2c7fbf39226faecb
tanya-agarwal06 <32618654+tanya-agarwal06@users.noreply.github.com>
2018-07-20 20:05:36 +0530

    icons done

--- text editor project/text edi.py
+++ text editor project/text edi.py
@@ -1,248 +1,221 @@
-import os
-import tkinter
-import tkinter.colorchooser
-from tkinter import ttk, filedialog, TclError
-from tkinter.scrolledtext import ScrolledText
-from tkinter.font import Font, families
-from tkinter import Tk
+from tkinter import *
 from tkinter.filedialog import *
 from tkinter import messagebox
 from tkinter import simpledialog
 import time
 import os
+import PIL
+from PIL import Image
 from tkinter.colorchooser import askcolor
 from tkinter import PhotoImage
+#===============================================functions=================================================================
 
-class Application(tkinter.Tk):
-    def __init__(self):
-        """Initialize widgets, methods."""
+filename=None
 
-        tkinter.Tk.__init__(self)
-        self.grid()
+def new_file(event=None):
+    global filename
+    filename="Untitled"
+    text.delete(0.0,END)
 
-        fontoptions = families(self)
-        font = Font(family="Verdana", size=10)
+def savefile(event=None):                               # file handling
+    global filename
+    t=text.get(0.0,END)
+    f=open(filename,mode='w')
+    f.write(t)
+    f.close()
 
-        menubar = tkinter.Menu(self)
-        fileMenu = tkinter.Menu(menubar, tearoff=0)
-        editMenu = tkinter.Menu(menubar, tearoff=0)
-        viewMenu = tkinter.Menu(menubar, tearoff=0)
-        fsubmenu = tkinter.Menu(editMenu, tearoff=0)
-        ssubmenu = tkinter.Menu(editMenu, tearoff=0)
-        hsubmenu = tkinter.Menu(editMenu, tearoff=0)
+def saveAs():
+    f=asksaveasfile(mode='w', defaultextension='.txt')
+    t=text.get(0.0,END)
+    try:
+        f.write(t.rstrip())
+    except:
+        showerror(title='oops',message='unable to save file')
 
-        # adds fonts to the font submenu and associates lambda functions
-        for option in fontoptions:
-            fsubmenu.add_command(label=option, command = lambda: font.configure(family=option))
-        # adds values to the size submenu and associates lambda functions
-        for value in range(1,31):
-            ssubmenu.add_command(label=str(value), command = lambda: font.configure(size=value))
+def openfile(event=None):
+    f=askopenfile(mode='r')
+    t=f.read()
+    text.delete(0.0,END)
+    text.insert(0.0,t)
 
-        # adds commands to the menus
-        menubar.add_cascade(label="File",underline=0, menu=fileMenu)
-        menubar.add_cascade(label="Edit",underline=0, menu=editMenu)
-        menubar.add_cascade(label="Help",underline=0, menu=viewMenu )
-        fileMenu.add_command(label="New", underline=1,
-                             command=self.new, accelerator="Ctrl+N")
-        fileMenu.add_command(label="Open", command=self.open, accelerator="Ctrl+O")
-        fileMenu.add_command(label="Save", command=self.save, accelerator="Ctrl+S")
-        fileMenu.add_command(label="Exit", underline=1,
-                             command=exit, accelerator="Ctrl+Q")
-        editMenu.add_command(label="Copy", command=self.copy, accelerator="Ctrl+C")
-        editMenu.add_command(label="Cut", command=self.cut, accelerator="Ctrl+X")
-        editMenu.add_command(label="Paste", command=self.paste, accelerator="Ctrl+V")
-        editMenu.add_cascade(label="Font", underline=0, menu=fsubmenu)
-        editMenu.add_cascade(label="Size", underline=0, menu=ssubmenu)
-        editMenu.add_command(label="Color", command=self.color)
-        editMenu.add_command(label="Bold", command=self.bold, accelerator="Ctrl+B")
-        editMenu.add_command(label="Italic", command=self.italic, accelerator="Ctrl+I")
-        editMenu.add_command(label="Underline", command=self.underline, accelerator="Ctrl+U")
-        editMenu.add_command(label="Overstrike", command=self.overstrike, accelerator="Ctrl+T")
-        editMenu.add_command(label="Undo", command=self.undo, accelerator="Ctrl+Z")
-        editMenu.add_command(label="Redo", command=self.redo, accelerator="Ctrl+Y")
-
-        self.config(menu=menubar)
-        self.bind_all("<Control-n>", self.new)
-        self.bind_all("<Control-o>", self.open)
-        self.bind_all("<Control-s>", self.save)
-        self.bind_all("<Control-q>", self.exit)
-        self.bind_all("<Control-b>", self.bold)
-        self.bind_all("<Control-i>", self.italic)
-        self.bind_all("<Control-u>", self.underline)
-        self.bind_all("<Control-T>", self.overstrike)
-        self.bind_all("<Control-z>", self.undo)
-        self.bind_all("<Control-y>", self.redo)
-
-        self.text = ScrolledText(self, state='normal', height=30, wrap='word', font = font, pady=2, padx=3, undo=True)
-        self.text.grid(column=0, row=0, sticky='NSEW')
-
-        # Frame configuration
-        self.grid_columnconfigure(0, weight=1)
-        self.resizable(True, True)
+def exit():
+    if messagebox.askyesno("Quit","Are you sure you want to quit"):
+        master.destroy()
 
 
-    def new_file(self, *args):
+def About():
+    label=messagebox.showinfo("about"," A python alternative to notepad")
 
-        app = Application()
-        app.title('Python Text Editor')
-        app.option_add('*tearOff', False)
-        app.mainloop()
+def findin(event=None):
+    findString =simpledialog.askstring('Find','Enter text')
+    t = text.get(0.0, END)
 
-    def color(self):
+    # count=0
+    occurences=t.upper().count(findString.upper())
+    if t.upper().count(findString.upper()) > 0:
+        label = messagebox.showinfo("RESULTS",findString +'has multiple occurences' + ""+ str(occurences))
+    else:
+        label = messagebox.showinfo('has nooccurences')
 
-        try:
-            (rgb, hx) = tkinter.colorchooser.askcolor()
-            self.text.tag_add('color', 'sel.first', 'sel.last')
-            self.text.tag_configure('color', foreground=hx)
-        except TclError:
-            pass
+    print(t.upper().count(findString.upper()))
 
-    def bold(self, *args):
-        try:
-            current_tags = self.text.tag_names("sel.first")
-            if "bold" in current_tags:
-                self.text.tag_remove("bold", "sel.first", "sel.last")
-            else:
-                self.text.tag_add("bold", "sel.first", "sel.last")
-                bold_font = Font(self.text, self.text.cget("font"))
-                bold_font.configure(weight="bold")
-                self.text.tag_configure("bold", font=bold_font)
-        except TclError:
-            pass
+#=======================================================================================================================
+def cut(event=None):
+    text.event_generate("<<Cut>>")
 
-    def italic(self, *args):
-        try:
-            current_tags = self.text.tag_names("sel.first")
-            if "italic" in current_tags:
-                self.text.tag_remove("italic", "sel.first", "sel.last")
-            else:
-                self.text.tag_add("italic", "sel.first", "sel.last")
-                italic_font = Font(self.text, self.text.cget("font"))
-                italic_font.configure(slant="italic")
-                self.text.tag_configure("italic", font=italic_font)
-        except TclError:
-            pass
+def copy(event=None):
+    text.event_generate("<<Copy>>")
 
-    def underline(self, *args):
-        try:
-            current_tags = self.text.tag_names("sel.first")
-            if "underline" in current_tags:
-                self.text.tag_remove("underline", "sel.first", "sel.last")
-            else:
-                self.text.tag_add("underline", "sel.first", "sel.last")
-                underline_font = Font(self.text, self.text.cget("font"))
-                underline_font.configure(underline=1)
-                self.text.tag_configure("underline", font=underline_font)
-        except TclError:
-            pass
+def paste(event=None):
+    text.event_generate("<<Paste>>")
 
-    def overstrike(self, *args):
-        try:
-            current_tags = self.text.tag_names("sel.first")
-            if "overstrike" in current_tags:
-                self.text.tag_remove("overstrike", "sel.first", "sel.last")
-            else:
-                self.text.tag_add("overstrike", "sel.first", "sel.last")
-                overstrike_font = Font(self.text, self.text.cget("font"))
-                overstrike_font.configure(overstrike=1)
-                self.text.tag_configure("overstrike", font=overstrike_font)
-        except TclError:
-            pass
+def delete(event=None):
+    text.delete(0.0,END)
 
-    def undo(self, *args):
-        try:
-            self.text.edit_undo()
-        except TclError:
-            pass
+def undo(event=None):
+    text.edit_undo()
 
-    def redo(self, *args):
-        try:
-            self.text.edit_redo()
-        except TclError:
-            pass
+def redo(event=None):
+    text.edit_redo()
 
-    def copy(self, *args):
-        self.clipboard_clear()
-        self.clipboard_append(self.text.selection_get())
+def selectAll(event=None):
+    text.tag_add('sel','0.0','end')
+    return'break'
+def deselectAll(event=None):
+    text.tag_remove('sel',0.0,'end')
+    return 'break'
 
-    def cut(self, *args):
-        self.copy
-        self.text.delete("sel.first", "sel.last")
 
-    def paste(self, *args):
-        insertion = self.selection_get(selection = "CLIPBOARD")
-        self.text.insert(0.0, insertion)
+def tim():
+    ti=time.gmtime()
+    text.clipboard_append(time.asctime(ti))
+    text.event_generate("<<Paste>>")
 
-    def open(self, *args):
-        filename = tkinter.filedialog.askopenfilename()
+def colors():
+        (triple, color) = askcolor()
+        if color:
+            text.config(foreground=color)
 
-        with open(filename) as f:
-            text = f.read()
+def normal():
+    text.config(font=("Times New Roman", 12))
 
-        self.text.delete("1.0", "end")
-        self.text.insert('insert', text)
+def bold():
+    text.config(font=("Times New Roman", 12, "bold"))
 
-    def save(self, *args):
-        try:
-            text = self.text.get("1.0", "end")
-            filename = tkinter.filedialog.asksaveasfilename()
+def italic():
+    text.config(font=("Times New Roman", 12, "italic"))
 
-            with open(filename, 'w') as f:
-                f.write(text)
-        except FileNotFoundError:
-            pass
+def underline():
+    text.config(font=("Times New Roman", 12, "underline"))
 
-    def exit(self, *args):
-        self.quit()
+def background():
+    (triple, color) = askcolor()
+    if color:
+        text.config(background=color)
+
+def help():
+    print("Help index")
+#================================================================================================================================
+
 master=Tk()
 #master.iconbitmap('icons/favicon.ico')
 scrollbar=Scrollbar(master)
 master.title("TEXT EDITOR")
+
 icon_frame=Frame(master,bg="black")
-icon_frame.pack(expand=NO,fill=X)
+icon_frame.pack(expand=NO,fill=X)                                       # user interface
 text=Text(master,yscrollcommand=scrollbar.set)
 text.pack(expand=YES,fill=BOTH)
 scrollbar.pack(side=RIGHT,fill=Y)
 scrollbar.config(command=text.yview())
 
-TopF=Frame(master)
+menu=Menu(master)
+master.config(menu=menu)
+filemenu=Menu(menu,tearoff=0)
+menu.add_cascade(label='file',menu=filemenu)
+filemenu.add_command(label='New',accelerator="Ctrl+N",command=new_file)
+filemenu.add_command(label='Open',accelerator="Ctrl+O",command=openfile)
+filemenu.add_command(label='Save',accelerator="Ctrl+S",command=savefile)
+filemenu.add_command(label='SaveAs',command=saveAs)
+filemenu.add_command(label='Find',accelerator="Ctrl+F",command=findin)
+filemenu.add_separator()
+filemenu.add_command(label="Quit",command=exit)
+editmenu=Menu(menu,tearoff=0)
+
+menu.add_cascade(label='Edit',menu=editmenu)
+editmenu.add_command(label='Undo',accelerator="Ctrl+Z",command=undo)
+editmenu.add_command(label='Redo',accelerator="Ctrl+Y",command=redo)
+editmenu.add_command(label='Cut',accelerator="Ctrl+X",command=cut)
+editmenu.add_command(label='Copy',accelerator="Ctrl+C",command=copy)
+editmenu.add_command(label='Paste',accelerator="Ctrl+V",command=paste)
+editmenu.add_command(label='Delete',accelerator="delete",command=delete)
+editmenu.add_separator()
+editmenu.add_command(label='Select All',accelerator="Ctrl+S",command=selectAll)
+editmenu.add_command(label='Deselect All',accelerator="Ctrl+D",command=deselectAll)
+editmenu.add_command(label='Time and Date',command=tim)
+formatmenu=Menu(menu,tearoff=0)
+menu.add_cascade(label='Format',menu=formatmenu)
+formatmenu.add_command(label='Colors',command=colors)
+formatmenu.add_separator()
+formatmenu.add_command(label='Font',command=normal)
+formatmenu.add_command(label='Bold',command=bold)
+formatmenu.add_command(label='Italic',command=italic)
+formatmenu.add_command(label='Underline',command=underline)
+formatmenu.add_separator()
+formatmenu.add_command(label='Background',command=background)
+
+helpmenu=Menu(menu)
+menu.add_cascade(label='Help',menu=helpmenu)
+helpmenu.add_command(label='Help',command=help)
+helpmenu.add_command(label='About',command=About)
+
+status=Label(master,text="PREP. to do nothing",bd=1,relief=SUNKEN,anchor=W)
+status.pack(side=BOTTOM,fill=X)
+#======================================================================================================================
+text.bind("<Control-N>",new_file)
+text.bind("<Control-O>",openfile)
+text.bind("<Control-S>",savefile)                             # SHORTCUT KEYS
+text.bind("<Control-F>",findin)
+text.bind("<Control-X>",cut)
+text.bind("<Control-C>",copy)
+text.bind("<Control-V>",paste)
+text.bind("<Control-A>",selectAll)
+text.bind("<Control-D>",deselectAll)
+#=======================================================================================================================
+img = Image.open("new_file.gif")
+width,height =img.size
+img.resize((100,100), Image.ANTIALIAS)
+
 new_file_icon = PhotoImage(file='new_file.gif')
-lab1=Label(TopF,image=new_file_icon)
-lab1.pack(side=LEFT)
-save_icon = PhotoImage(file='save1.gif')
-lab2=Label(TopF,image=save_icon)
-lab2.pack(side=LEFT)
+labe1=Button(master,image=new_file_icon)
+labe1.pack(side=LEFT)
+savefile_icon = PhotoImage(file='save.gif')
+labe2=Button(master,image=savefile_icon)
+labe2.pack(side=LEFT)
 cut_icon = PhotoImage(file='cut1.gif')
-lab3=Label(TopF,image=cut_icon)
-lab3.pack(side=LEFT)
+labe3=Button(master,image=cut_icon)
+labe3.pack(side=LEFT)
 copy_icon = PhotoImage(file='copy.gif')
-lab4=Label(TopF,image=copy_icon)
-lab4.pack(side=LEFT)
-paste_icon = PhotoImage(file='paste1.gif')
-lab5=Label(TopF,image=paste_icon)
-lab5.pack(side=LEFT)
+labe4=Button(master,image=copy_icon)
+labe4.pack(side=LEFT)
+paste_icon = PhotoImage(file='paste1.gif')                # ICONS
+labe5=Button(master,image=paste_icon)
+labe5.pack(side=LEFT)
 undo_icon = PhotoImage(file='undo1.gif')
-lab6=Label(TopF,image=undo_icon)
-lab6.pack(side=LEFT)
+labe6=Button(master,image=undo_icon)
+labe6.pack(side=LEFT)
 redo_icon = PhotoImage(file='redo1.gif')
-lab7=Label(TopF,image=redo_icon)
-lab7.pack(side=LEFT)
+labe7=Button(master,image=redo_icon)
+labe7.pack(side=LEFT)
 
+#D:\assignment\icons\cut.gif
 #adding shortcut icons
-icons=( 'new_file','save', 'cut', 'copy', 'paste','undo', 'redo')
-for i, icon in enumerate(icons):
-    tool_bar_icon = PhotoImage(file='icons/{}.gif'.format(icon)).zoom(2,2)
-    cmd = eval(icon)
-    tool_bar = Button(icon_frame, image=tool_bar_icon, height=35,width=35, command=cmd)
-    tool_bar.image = tool_bar_icon
-    tool_bar.pack(side=LEFT)
+# icons=( 'new_file','save', 'cut1', 'copy1', 'paste1','undo1', 'redo1')
+# for i, icon in enumerate(icons):
+#     tool_bar_icon = PhotoImage(file='{}.gif'.format(icon)).zoom(2,2)
+#     cmd = eval(icon)
+#     tool_bar = Button(icon_frame, image=tool_bar_icon, height=35,width=35, command=cmd)
+#     tool_bar.image = tool_bar_icon
+#     tool_bar.pack(side=LEFT)
 
-
-
-if __name__ == "__main__":
-    app=Application()
-    app.title('Python Text Editor')
-    app.option_add('*tearOff', False)
-    app.mainloop()
-
-
+mainloop()
